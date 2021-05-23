54b9a3f4668b6583a8983ad45bcf11ab4ee01906
thebinary <binary4bytes@gmail.com>
2021-05-23 17:21:32 +0545

    add .emacs.d and configuration.org within

---
+++ .emacs.d/init.el
@@ -0,0 +1,15 @@
+(load-file (expand-file-name "~/.emacs.d/configuration.el"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(counsel-projectile dockerfile-mode magit org-tree-slide counsel engine-mode org-bullets all-the-icons-dired which-key doom-modeline doom-themes use-package exec-path-from-shell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
